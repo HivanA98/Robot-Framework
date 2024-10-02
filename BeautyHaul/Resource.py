9b9dde5e035e0c8501181284809a7c84da64b15d
Ivan Armadi <103761586+HivanA98@users.noreply.github.com>
2024-10-02 22:29:17 +0700

    Robot_BeautyHaul-V.0.0.1-Resourch

---
+++ BeautyHaul/Resource.py
@@ -0,0 +1,12 @@
+from robot.api.logger import info, debug, trace, console
+
+URL = "https://www.beautyhaul.com"
+
+UsernameLogin = "[class='form-control form-control-email']"
+PasswordLogin = "[name='password']"
+NormalUser = "Ivan"
+NormalPass = "Password!2#"
+UnnormalUser = "N@"
+UnnormalPass = "pwd"
+BtnSignin = "[class='btn btn-block btn-primary']"
+ErrorWarning = '[class="error invalid-feedback"]'
